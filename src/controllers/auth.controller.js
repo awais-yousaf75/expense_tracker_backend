01f910d30ebd257f7168d0183ace2cd84a876f95
Awais Yousaf <awaisyousaf75@gmail.com>
2026-03-22 19:52:29 +0500

    refresh token implemented

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,49 +4,172 @@ import config from "../config/config.js";
 import bcrypt from "bcrypt";
 
 export async function registerUser(req, res) {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const isExist = await userModel.findOne({
-    email,
-  });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
-  if (isExist) {
-    return res.status(422).json({
-      message: "User with this email already exists.",
-      status: "Failed",
+    const isExist = await userModel.findOne({ email });
+
+    if (isExist) {
+      return res.status(409).json({
+        message: "User with this email already exists.",
+        status: "Failed",
+      });
+    }
+
+    const user = await userModel.create({ username, email, password });
+
+    const refreshToken = jwt.sign(
+      {
+        userId: user._id,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(201).json({
+      message: "User registered successfully.",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+      accessToken,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+}
+
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isPasswordConfirmed = await user.comparePassword(password);
+
+    if (!isPasswordConfirmed) {
+      return res.status(401).json({
+        message: "Invalid credentials.",
+      });
+    }
+
+    const refreshToken = jwt.sign(
+      {
+        userId: user._id,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully.",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+      accessToken,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+}
+
+export async function refreshToken(req, res) {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(400).json({
+      message: "Refresh token not found.",
     });
   }
 
-  const user = await userModel.create({ username, email, password });
+  const decoded = jwt.verify(refreshToken, config.JWT_SECRET);
 
-  //   const refreshToken = jwt.sign(
-  //     {
-  //       userId: user._id,
-  //     },
-  //     config.JWT_SECRET,
-  //     {
-  //       expiresIn: "7d",
-  //     },
-  //   );
+  const accessToken = jwt.sign(
+    {
+      id: decoded.id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
 
-  //   const refreshTokenHash = await bcrypt.hash("refreshToken", 10);
+  const newRefreshToken = jwt.sign(
+    {
+      id: decoded.id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
 
-  //   const accessToken = jwt.sign(
-  //     {
-  //       userId: user._id,
-  //     },
-  //     config.JWT_SECRET,
-  //     {
-  //       expiresIn: "7d",
-  //     },
-  //   );
+  res.cookie("refreshToken", newRefreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    expiresIn: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
 
   res.status(200).json({
-    message: "User registered successfully.",
-    user: {
-      username: user.username,
-      email: user.email,
-    },
+    message: "Access token refreshed successfully.",
+    accessToken,
   });
 }
-
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,6 +7,13 @@ const authRouter = Router();
  * POST /api/auth/register
  */
 authRouter.post("/register", authController.registerUser);
-
+/**
+ * POST /api/auth/login
+ */
+authRouter.post("/login", authController.loginUser);
+/**
+ * POST /api/auth/refresh-token
+ */
+authRouter.post("/refresh-token", authController.refreshToken);
 
 export default authRouter;
